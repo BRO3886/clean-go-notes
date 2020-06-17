c4a9f6c17e65de6fd015b259b8fe8477c6ad53fa
BRO3886 <siddverma1999@gmail.com>
2020-06-17 15:12:42 +0530

    deploy

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/BRO3886/clean-go-notes/pkg/note"
 
@@ -11,18 +12,34 @@ import (
 	"github.com/BRO3886/clean-go-notes/pkg/user"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+func connectDB() (*gorm.DB, error) {
+
+	//Heroku
+	if os.Getenv("DATABASE_URL") != "" {
+		return gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	}
+	return gorm.Open("sqlite3", "test.db")
+}
+
 func main() {
 	log.Println("start test server")
-	db, err := gorm.Open("sqlite3", "test.db")
+
+	db, err := connectDB()
 	if err != nil {
 		log.Fatal("error conneting to DB")
 	}
-	defer db.Close()
 	log.Println("connected to db")
-	db.LogMode(false)
+
+	defer db.Close()
+
+	if os.Getenv("DB_LOG_MODE") == "true" {
+		db.LogMode(true)
+	}
+
 	db.AutoMigrate(&user.User{}, &note.Note{})
 
 	userRepo := user.NewSqliteRepo(db)
@@ -36,11 +53,20 @@ func main() {
 	handler.MakeUserHandlers(myRouter, userSvc)
 	handler.MakeNotesHandler(myRouter, noteSvc)
 
+	//health route
 	myRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("Helth"))
+		_, _ = w.Write([]byte("ping"))
 		return
 	})
-	fmt.Println("All ok. Serving...")
-	log.Fatal(http.ListenAndServe("localhost:3000", myRouter))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1729"
+	}
+
+	log.Println("All ok. Serving on port=", port)
+
+	// log.Fatal(http.ListenAndServe("localhost:3000", myRouter))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s", port), myRouter))
 }
